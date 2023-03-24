5364ae0c85444e852661969e0543773b940d0a95
Oriol <oriolginger2008@gmail.com>
2023-03-24 12:30:32 +0100

    7 finished and 8 created

--- Codelearn/src/Array7.java
+++ Codelearn/src/Array7.java
@@ -1,4 +1,19 @@
 public class Array7 {
-	int[][] table = new int[10][10];
-	
+	public static void main(String[] args) {
+		int[][] array = new int[5][5];  // se crea una matriz de 5x5 enteros
+    
+    // se llena la matriz con la suma de las coordenadas n y m
+    for (int n = 0; n < 5; n++) {
+        for (int m = 0; m < 5; m++) {
+            array[n][m] = n + m;
+        }
+    }
+    
+    // se muestra el contenido de la matriz
+    for (int n = 0; n < 5; n++) {
+        for (int m = 0; m < 5; m++) {
+            System.out.println("Value of the position (" + n + ", " + m + "): " + array[n][m]);
+        }
+    }
+	}
 }

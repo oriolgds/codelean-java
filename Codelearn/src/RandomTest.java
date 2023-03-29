588ab4dab5d0b0bc305e3e56f9e4054eb8460c30
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-03-29 17:09:16 +0200

    Random test finished

---
+++ Codelearn/src/RandomTest.java
@@ -0,0 +1,20 @@
+package exercices.java;
+import java.util.Random;
+
+public class RandomTest {
+    Random r = new Random();
+    int[][] array = new int[3][3];
+    
+    // Llenar la matriz
+    for (int n = 0; n < array.length; n++) {
+        for (int m = 0; m < array[n].length; m++) {
+            array[n][m] = (r.nextInt() * 100 - 1);
+        }
+    }
+    // Mostrar los valores de la matriz
+    for (int n = 0; n < array.length; n++) {
+        for (int m = 0; m < array[n].length; m++) {
+            System.out.print("Value of the position (" + n + ", " + m + "): " + array[n][m]);
+        }
+    }    
+}

441bcd3b1981eb7fb9e58fb76ce31f75b16975d6
Oriol <oriolginger2008@gmail.com>
2023-03-30 09:52:04 +0200

    Function main created

--- Codelearn/src/RandomTest.java
+++ Codelearn/src/RandomTest.java
@@ -1,20 +1,23 @@
-package exercices.java;
+//package exercices.java;
 import java.util.Random;
 
 public class RandomTest {
-    Random r = new Random();
-    int[][] array = new int[3][3];
+    public static void main(String[] args) {
+        Random r = new Random();
+        int[][] array = new int[3][3];
     
-    // Llenar la matriz
-    for (int n = 0; n < array.length; n++) {
-        for (int m = 0; m < array[n].length; m++) {
-            array[n][m] = (r.nextInt() * 100 - 1);
+        // Llenar la matriz
+        for (int n = 0; n < array.length; n++) {
+            for (int m = 0; m < array[n].length; m++) {
+                array[n][m] = (r.nextInt() * 100 - 1);
+            }
         }
+        // Mostrar los valores de la matriz
+        for (int n = 0; n < array.length; n++) {
+            for (int m = 0; m < array[n].length; m++) {
+                System.out.print("Value of the position (" + n + ", " + m + "): " + array[n][m]);
+            }
+        } 
     }
-    // Mostrar los valores de la matriz
-    for (int n = 0; n < array.length; n++) {
-        for (int m = 0; m < array[n].length; m++) {
-            System.out.print("Value of the position (" + n + ", " + m + "): " + array[n][m]);
-        }
-    }    
+       
 }

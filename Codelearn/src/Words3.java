1e57d3b4af14843e6543b75d417f2e06d98698c7
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-04-26 17:16:46 +0200

    Finished but not debugged

---
+++ Codelearn/src/Words3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Words3 {
+    private static String removeWords(String in){
+        return in.substring(in.indexOf(" ") + 1, in.lastIndexOf(" "));
+    }  
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String[] array = new String[5];
+		for(int i = 0; i < 5; i++){
+            System.out.print("Insert a phrase " + i + ": ");
+            array[i] = removeWords(sc.nextLine());
+        }
+        for(String s : array){
+            System.out.println(s);
+        }
+	}
+}

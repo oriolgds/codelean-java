07099ef5ef96295ddd1340c6c4c3cff716bf0818
Oriol <oriolginger2008@gmail.com>
2023-04-26 12:34:12 +0200

    Finished but with errors

--- Codelearn/src/Words1.java
+++ Codelearn/src/Words1.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
 public class Words1 {
 	private static String funar(String aFunar){
 		aFunar = aFunar.trim();
-		aFunar = aFunar.substring(0, aFunar.lastIndexOf(" ", 0));
+		aFunar = aFunar.substring(0, aFunar.lastIndexOf(" "));
 		return aFunar;
 	}
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Insert a phrase: ");
+		String phrase = sc.nextLine();
+		String result = funar(phrase);
+		System.out.print("The given phrase after deleting the last word is " + result);
 	}
 }

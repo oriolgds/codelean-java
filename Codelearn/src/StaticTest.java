aa29d15466459bcd412744dd242df43b9f8dde24
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-05-01 16:41:13 +0200

    Ejercicio Java 77 (Static test) finished and corrected

---
+++ Codelearn/src/StaticTest.java
@@ -0,0 +1,8 @@
+public class StaticTest {
+    private static String name = "Codelearn";
+    private static int year = 2013;
+    public static void main(String[] args) {
+        System.out.println("Value of the integer: " + StaticTest.year);
+        System.out.println("Value of text: " + StaticTest.name);
+    }
+}

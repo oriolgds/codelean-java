6270ef4b5f4db698907bca274c1a98c77ec81489
Oriol <oriolginger2008@gmail.com>
2023-05-04 15:29:09 +0200

    Setters and getters

--- Codelearn/src/Electrodomestic/Electrodomestic.java
+++ Codelearn/src/Electrodomestic/Electrodomestic.java
@@ -1,5 +1,8 @@
 package Electrodomestic;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Electrodomestic {
     private double _basePrice;
     public double get_basePrice(){
@@ -38,9 +41,62 @@ public class Electrodomestic {
         _color = "white";
     }
     Electrodomestic(double price, double weight, char energyConsumption, String color){
-        _basePrice = price;
         _weight = weight;
-        _energyConsumption = energyConsumption;
-        _color = color;
+        _energyConsumption = this.checkEnergyConsumption(energyConsumption);
+        _color = this.checkColor(color);
+        _basePrice = this.finalPrice(_basePrice);
+
+    }
+    double finalPrice(double price){
+        char l = this._energyConsumption;
+        if(l == 'A'){
+            price += 100;
+        } else if(l == 'B'){
+            price += 80;
+        } else if(l == 'C'){
+            price += 60;
+        } else if(l == 'D'){
+            price += 50;
+        } else if(l == 'E'){
+            price += 30;
+        } else if(l == 'F'){
+            price += 10;
+        }
+        if(this._weight > 0 && this._weight < 20){
+            price += 10;
+        } else if(this._weight >= 20 && this._weight < 50){
+            price += 50;
+        } else if(this._weight >= 50 && this._weight < 80){
+            price += 80;
+        } else if (this._weight > 80){
+            price += 100;
+        }
+        return price;
+    }
+    // Methods
+    char checkEnergyConsumption(char letter){
+        // Check if it's a valid letter
+        if(letter >= 'A' && letter <= 'F'){
+            return letter;
+        } else {
+            return 'F';
+        }
+    }
+    String checkColor(String color){
+        String[] colorsArray = {"white", "black", "red", "blue", "grey"};
+        List<String> colors = List.of(Arrays.toString(colorsArray));
+        if(colors.contains(color)){
+            return color;
+        } else {
+            return "white";
+        }
+    }
+    public String toString(){
+        System.out.println("Price: " + this._basePrice);
+        System.out.println("Color: " + this._color);
+        System.out.println("EnergyConsumption: " + this._energyConsumption);
+        System.out.println("Weight: " + this._weight);
+        System.out.println("************************************************");
+        return null;
     }
 }

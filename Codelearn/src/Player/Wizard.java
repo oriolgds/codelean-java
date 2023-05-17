40363acdb6c173ab8b52f2efa27c31a65a769ebe
Oriol <oriolginger2008@gmail.com>
2023-05-17 17:28:01 +0200

    Wizard in progress

--- Codelearn/src/Player/Wizard.java
+++ Codelearn/src/Player/Wizard.java
@@ -1,6 +1,22 @@
 package Player;
 
 public class Wizard extends Player{
+    public int getMP() {
+        return MP;
+    }
+
+    public void setMP(int MP) {
+        this.MP = MP;
+    }
+
+    public int getMPoints() {
+        return MPoints;
+    }
+
+    public void setMPoints(int MPoints) {
+        this.MPoints = MPoints;
+    }
+
     protected int MP;
     protected int MPoints;
     protected int avaibleMPoints;
@@ -9,14 +25,25 @@ public class Wizard extends Player{
 
     Wizard(String _playerName, String _characterName) {
         super(_playerName, _characterName);
+        this.MP = BaseMP;
+        this.MPoints = BaseMPoints;
     }
 
-    Wizard(String _playerName, String _characterName, int _baseLevel, int _baseXP, int _baseHP) {
+    Wizard(String _playerName, String _characterName, int _baseLevel, int _baseXP, int _baseHP, int MP, int MPoints) {
         super(_playerName, _characterName, _baseLevel, _baseXP, _baseHP);
+        this.MP = MP;
+        this.MPoints = MPoints;
     }
 
     @Override
     public void gainXp(int value) {
-
+        XP += value;
+        int c = level * XP;
+        if(c > level * 1000 * level / 2){
+            level++;
+            MP++;
+            MPoints += 5;
+            this._HP += 5;
+        }
     }
 }

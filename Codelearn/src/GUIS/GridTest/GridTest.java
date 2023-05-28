459709a9a911bfeed8947023bb4fbc344eae2b8d
Oriol <oriolginger2008@gmail.com>
2023-05-28 19:18:28 +0200

    Grid test finished

--- Codelearn/src/GUIS/GridTest/GridTest.java
+++ Codelearn/src/GUIS/GridTest/GridTest.java
@@ -1,9 +1,22 @@
 package GUIS.GridTest;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GridTest extends JFrame {
     GridTest(String title){
         super(title);
+        super.setSize(400, 200);
+        super.setVisible(true);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container cp = getContentPane();
+        GridLayout gridLayout = new GridLayout(1, 3);
+        gridLayout.setHgap(5);
+        gridLayout.setVgap(5);
+        cp.setLayout(gridLayout);
+        cp.add(new JButton("First button"));
+        cp.add(new JButton("Second button"));
+        cp.add(new JButton("Third button"));
+
     }
 }
--- Codelearn/src/GUIS/GridTest/Main.java
+++ Codelearn/src/GUIS/GridTest/Main.java
@@ -3,7 +3,6 @@ package GUIS.GridTest;
 public class Main {
     public static void main(String[] args) {
         GridTest g = new GridTest("Grid Window Example");
-        g.setSize(100, 400);
         g.setVisible(true);
     }
 }

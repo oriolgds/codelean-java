f242c77091a38e667c3ead423a4134c31f8badf6
Oriol <110051760+oriolgds@users.noreply.github.com>
2023-05-30 20:12:40 +0200

    JRadioButtonExample finished and send

--- Codelearn/src/GUIS/JRadioButtonExample/JRadioButtonExample.java
+++ Codelearn/src/GUIS/JRadioButtonExample/JRadioButtonExample.java
@@ -12,9 +12,30 @@ public class JRadioButtonExample extends JFrame {
 
 
         Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+        Container dataPanel = new Container ();
+        dataPanel.setLayout(new FlowLayout());
+        dataPanel.add ( new JLabel ( "Username:" ) );
+        dataPanel.add ( new JTextField ( 15 ) );
+        cp.add(dataPanel, BorderLayout.NORTH);
 
+        String[] programmingLanguages = new String[]{"Python", "Java", "C++", "C#"};
+        Container cp2 = new Container();
+        GridLayout g1 = new GridLayout(programmingLanguages.length, 1);
+        g1.setVgap(20);
+        cp2.setLayout(g1);
+        for (String pl:
+             programmingLanguages) {
+            cp2.add(new JRadioButton(pl));
+        }
+        cp.add(cp2, BorderLayout.CENTER);
 
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 
+        buttonPanel.add(new JButton("Accept"));
+        buttonPanel.add(new JButton("Cancel"));
+        cp.add(buttonPanel, BorderLayout.SOUTH);
         setVisible(true);
     }
 }
--- Codelearn/src/GUIS/JRadioButtonExample/Main.java
+++ Codelearn/src/GUIS/JRadioButtonExample/Main.java
@@ -2,6 +2,6 @@ package GUIS.JRadioButtonExample;
 
 public class Main {
     public static void main(String[] args) {
-        JRadioButtonExample r = new JRadioButtonExample();
+        JRadioButtonExample r = new JRadioButtonExample("Radio Button Window", 300, 400);
     }
 }

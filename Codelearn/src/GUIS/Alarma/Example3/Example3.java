f924cc885dade42cbbfb6ffc424fddeb139d8b54
Oriol <oriolginger2008@gmail.com>
2023-05-31 17:30:38 +0200

    Example3 started

--- Codelearn/src/GUIS/Alarma/Example3/Example3.java
+++ Codelearn/src/GUIS/Alarma/Example3/Example3.java
@@ -21,11 +21,39 @@ public class Example3 extends JFrame {
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Crear contenedor
+        Container cp = getContentPane();
+        cp.setLayout(new GridLayout(3, 1));
 
+        // Crear los JPanel
+        JPanel textPane = new JPanel();
+        JPanel buttonPane = new JPanel();
 
+        // Iniciando atributos (textField)
+        txf1 = new JTextField(10);
+        txf2 = new JTextField(10);
 
+        // Iniciando atributos (botones)
+        sum = new JButton("Sum");
+        sub = new JButton("Substract");
 
-
+        // ActionListeners
+        sum.addActionListener(new ActionSum());
         setVisible(true);
     }
+
+    public void actionSum ( ActionEvent e ) {
+        int a, b;
+
+        a = Integer.parseInt(txf1.getText());
+
+        b = Integer.parseInt(txf2.getText());
+
+        lab.setText("The sum of " + txf1.getText() + " and " + txf2.getText() + " is " + String.valueOf(a+b));
+    }
+
+    public static void main(String[] args) {
+        Example3 example3 = new Example3("Third example", 350, 125);
+    }
 }
+

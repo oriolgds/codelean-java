245a1df5c72f60784853a17eccef5cb41852b451
Oriol <oriolginger2008@gmail.com>
2023-06-06 10:45:06 +0200

    Dialog 2 started

--- Codelearn/src/GUIS/Dialogs/dialog2.java
+++ Codelearn/src/GUIS/Dialogs/dialog2.java
@@ -1,4 +1,54 @@
 package GUIS.Dialogs;
 
-public class dialog2 {
+import javax.swing.*;
+
+import java.awt.*;
+
+import java.awt.event.ActionListener;
+
+import java.awt.event.ActionEvent;
+
+public class dialog2 extends JFrame {
+    private JLabel question;
+    private JButton bt;
+    dialog2(){
+        super("Dialog 2 example");
+        setSize(new Dimension(350, 75));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create container
+        Container cp = getContentPane();
+        cp.setLayout(new FlowLayout());
+
+        // Initialize attributes
+        question = new JLabel("What's your name? ");
+        bt = new JButton("Answer");
+
+
+        setVisible(true);
+    }
+
+
+    private class showDialog implements ActionListener {
+        @Override
+        public void actionPerformed ( ActionEvent e ) {
+
+            JButton b = (JButton) e.getSource ();
+
+            String name = JOptionPane.showInputDialog ( b.getParent (), "Enter your name:", "");
+
+            if ( name.equals("") ) {
+
+                question.setText ( "Are you sure you have written your name correctly?" );
+
+            } else {
+
+                question.setText ( "Nice to meet you " + name + "!" );
+
+                bt.setVisible ( false );
+
+            }
+
+        }
+    }
 }
